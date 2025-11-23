8a315545b72b5ee16934139f0f6ed02b1948e1bd
Baraa RJ <baraarjoob17@gmail.com>
2025-11-23 13:13:32 +0200

    completing the Adapter

--- app/src/main/java/adapter/TripAdapter.java
+++ app/src/main/java/adapter/TripAdapter.java
@@ -1,6 +1,26 @@
 package adapter;
 
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class TripAdapter extends RecyclerView.Adapter<> {
+import viewholder.TripViewHolder;
+
+public class TripAdapter extends RecyclerView.Adapter<TripViewHolder> {
+    @NonNull
+    @Override
+    public TripViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TripViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
--- app/src/main/java/viewholder/TripViewHolder.java
+++ app/src/main/java/viewholder/TripViewHolder.java
@@ -6,13 +6,41 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.myapplication.R;
+
 public class TripViewHolder extends RecyclerView.ViewHolder {
     private TextView tripNameTextView;
     private TextView tripDateTextView;
     private TextView tripDescriptionTextView;
+    private OnItemClickListener listener;
 
-    public TripViewHolder(@NonNull View itemView) {
+    public interface OnItemClickListener {
+        void onItemClick(int position);
+    }
+
+    public TripViewHolder(@NonNull View itemView, OnItemClickListener listener) {
         super(itemView);
+        this.listener = listener;
+        initializeViews();
+        itemView.setOnClickListener(v -> {
+            if (listener != null) {
+                int position = getBindingAdapterPosition();
+                if (position != RecyclerView.NO_POSITION) {
+                    listener.onItemClick(position);
+                }
+            }
+        });
+    }
 
+    public void bindData(String name, String date, String description) {
+        tripNameTextView.setText(name);
+        tripDateTextView.setText(date);
+        tripDescriptionTextView.setText(description);
+    }
+
+    private void initializeViews() {
+        tripDateTextView = itemView.findViewById(R.id.textViewDate);
+        tripNameTextView = itemView.findViewById(R.id.textViewTitle);
+        tripDescriptionTextView = itemView.findViewById(R.id.textViewDescription);
     }
 }
