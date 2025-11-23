ab69cc30e33b63b466f67f54faecc1e8103243e9
Baraa RJ <baraarjoob17@gmail.com>
2025-11-23 12:46:50 +0200

    Logic For the Second Activity

--- app/src/main/java/com/example/myapplication/activities/ViewTripActivity.java
+++ app/src/main/java/com/example/myapplication/activities/ViewTripActivity.java
@@ -1,4 +1,26 @@
 package com.example.myapplication.activities;
 
-public class ViewTripActivity {
+import android.os.Bundle;
+import android.widget.RadioGroup;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.myapplication.R;
+
+public class ViewTripActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RadioGroup radioGroup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_trip);
+        initializeViews();
+    }
+
+    private void initializeViews() {
+        recyclerView = findViewById(R.id.recyclerViewTrips);
+        radioGroup = findViewById(R.id.radioGroupTrips);
+    }
+
 }
